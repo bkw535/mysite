f4bb27d99e12178cbd49855b1b3e45b2dcc32821
bkw535 <bkw54570@gmail.com>
2024-05-31 01:32:09 +0900

    add test code

--- polls/views.py
+++ polls/views.py
@@ -8,6 +8,7 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from .models import Choice, Question
 from django.views import generic
+from django.utils import timezone
 
 # Create your views here.
 # def index(request):
@@ -38,6 +39,15 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         """Return the last five published questions."""
         return Question.objects.order_by("-pub_date")[:5]
+    
+def get_queryset(self):
+    """
+    Return the last five published questions (not including those set to be
+    published in the future).
+    """
+    return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
+        :5
+    ]
 
 
 # def detail(request, question_id):
